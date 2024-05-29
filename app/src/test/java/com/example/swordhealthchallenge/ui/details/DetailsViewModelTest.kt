190b2efa65f8a2eb8c34b30dec90ed5ebadee2b7
Rui Correia <rui-manuel-18@hotmail.com>
2024-05-29 12:12:14 +0100

    DetailsViewModel unit tests

---
+++ app/src/test/java/com/example/swordhealthchallenge/ui/details/DetailsViewModelTest.kt
@@ -0,0 +1,77 @@
+package com.example.swordhealthchallenge.ui.details
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.example.swordhealthchallenge.domain.usecases.DeleteFavouriteCatUseCase
+import com.example.swordhealthchallenge.domain.usecases.GetCatDetailsUseCase
+import com.example.swordhealthchallenge.domain.usecases.PostFavouriteCatUseCase
+import com.example.swordhealthchallenge.ui.utils.CatDetailsDomainModelFakes.catDetailsFav1
+import com.example.swordhealthchallenge.ui.utils.CatDetailsDomainModelFakes.catDetailsNoFav1
+import io.mockk.every
+import io.mockk.mockk
+import io.reactivex.rxjava3.core.Completable
+import io.reactivex.rxjava3.core.Scheduler
+import io.reactivex.rxjava3.core.Single
+import io.reactivex.rxjava3.schedulers.Schedulers
+import org.junit.Assert
+import org.junit.Rule
+import org.junit.Test
+
+class DetailsViewModelTest {
+    private val getCatDetailsUseCase: GetCatDetailsUseCase = mockk()
+    private val postFavouriteCatUseCase: PostFavouriteCatUseCase = mockk()
+    private val deleteFavouriteCatUseCase: DeleteFavouriteCatUseCase = mockk()
+    private val ioSchedulers: Scheduler = Schedulers.trampoline()
+    private val mainSchedulers: Scheduler = Schedulers.trampoline()
+    private val viewModel = DetailsViewModel(
+        getCatDetailsUseCase,
+        postFavouriteCatUseCase,
+        deleteFavouriteCatUseCase,
+        ioSchedulers,
+        mainSchedulers
+    )
+
+    @get:Rule
+    val rule = InstantTaskExecutorRule()
+
+    @Test
+    fun `when getCatDetails success should fill livedata`() {
+        val expected = catDetailsNoFav1
+
+
+        every { getCatDetailsUseCase.getCatDetails("1") } returns Single.just(expected)
+        viewModel.getCatDetails("1", "URL1", "")
+
+        Assert.assertEquals(expected, viewModel.catDetails.value)
+    }
+
+    @Test
+    fun `when favouriteCat success should update livedata`() {
+        val favId = "FavId1"
+        val imageId = "ImageId1"
+        val expected = catDetailsFav1
+
+        every { postFavouriteCatUseCase.postFavouriteCat(imageId) } returns Single.just(favId)
+        every { getCatDetailsUseCase.getCatDetails("1") } returns Single.just(expected)
+        viewModel.getCatDetails("1", "URL1", favId)
+
+        viewModel.favouriteCat(imageId)
+
+        Assert.assertEquals(expected, viewModel.catDetails.value)
+    }
+
+    @Test
+    fun `when deleteFavouriteCat success should update livedata`() {
+        val favId = "FavId1"
+        val imageId = "ImageId1"
+        val expected = catDetailsNoFav1
+
+        every { deleteFavouriteCatUseCase.deleteFavouriteCat(imageId) } returns Completable.complete()
+        every { getCatDetailsUseCase.getCatDetails("1") } returns Single.just(expected)
+        viewModel.getCatDetails("1", "URL1", favId)
+
+        viewModel.deleteFavouriteCat(imageId)
+
+        Assert.assertEquals(expected, viewModel.catDetails.value)
+    }
+
+}
---
+++ app/src/test/java/com/example/swordhealthchallenge/ui/utils/CatDetailsDomainModelFakes.kt
@@ -0,0 +1,26 @@
+package com.example.swordhealthchallenge.ui.utils
+
+import com.example.swordhealthchallenge.domain.Model.CatDetails
+
+object CatDetailsDomainModelFakes {
+    val catDetailsNoFav1 = CatDetails(
+        id = "1",
+        breed = "Breed1",
+        imageUrl = "URL1",
+        imageId = "ImageId1",
+        favouriteId = "",
+        origin = "Origin1",
+        temperament = "Temperament1",
+        description = "Description1",
+    )
+    val catDetailsFav1 = CatDetails(
+        id = "1",
+        breed = "Breed1",
+        imageUrl = "URL1",
+        imageId = "ImageId1",
+        favouriteId = "FavId1",
+        origin = "Origin1",
+        temperament = "Temperament1",
+        description = "Description1",
+    )
+}
