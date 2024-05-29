39a40c3efab4e9b7a584dc60f5f0feb33afa99d1
Rui Correia <rui-manuel-18@hotmail.com>
2024-05-29 14:27:57 +0100

    moved @Headers from api to retrofit builder

--- app/src/main/java/com/example/swordhealthchallenge/data/CatApi.kt
+++ app/src/main/java/com/example/swordhealthchallenge/data/CatApi.kt
@@ -1,6 +1,5 @@
 package com.example.swordhealthchallenge.data
 
-import com.example.swordhealthchallenge.BuildConfig
 import com.example.swordhealthchallenge.data.entities.CatByImageResponse
 import com.example.swordhealthchallenge.data.entities.CatDetailsResponse
 import com.example.swordhealthchallenge.data.entities.CatResponse
@@ -12,28 +11,23 @@ import io.reactivex.rxjava3.core.Single
 import retrofit2.http.Body
 import retrofit2.http.DELETE
 import retrofit2.http.GET
-import retrofit2.http.Headers
 import retrofit2.http.POST
 import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface CatApi {
 
-    @Headers("x-api-key: ${BuildConfig.API_KEY}")
     @GET("breeds")
     fun getCatList(
         @Query("limit") limit: String = "20",
     ): Single<List<CatResponse>>
 
-    @Headers("x-api-key: ${BuildConfig.API_KEY}")
     @GET("breeds/search")
     fun searchCat(@Query("q") search: String): Single<List<CatResponse>>
 
-    @Headers("x-api-key: ${BuildConfig.API_KEY}")
     @GET("favourites")
     fun getFavouriteCats(): Single<List<FavouriteCatResponse>>
 
-    @Headers("x-api-key: ${BuildConfig.API_KEY}")
     @POST("favourites")
     fun postFavouriteCat(@Body imageId: FavouriteCatBody): Single<PostFavouriteResponse>
 
@@ -43,7 +37,6 @@ interface CatApi {
     @GET("breeds/{breedsId}")
     fun getCatDetails(@Path("breedsId") catId: String): Single<CatDetailsResponse>
 
-    @Headers("x-api-key: ${BuildConfig.API_KEY}")
     @DELETE("favourites/{favouriteId}")
     fun deleteFavourite(@Path("favouriteId") favouriteId: String): Completable
 }
--- app/src/main/java/com/example/swordhealthchallenge/di/AppModule.kt
+++ app/src/main/java/com/example/swordhealthchallenge/di/AppModule.kt
@@ -8,19 +8,38 @@ import hu.akarnokd.rxjava3.retrofit.RxJava3CallAdapterFactory
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.core.Scheduler
 import io.reactivex.rxjava3.schedulers.Schedulers
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Named
+import javax.inject.Singleton
 
 @Module
 class AppModule {
 
+    @Singleton
     @Provides
-    fun provideRetrofit(): CatApi {
+    fun provideHttpClient(): OkHttpClient {
+        val interceptor = Interceptor { chain ->
+            val originalRequest = chain.request()
+            val newRequest = originalRequest.newBuilder()
+                .header("x-api-key", BuildConfig.API_KEY)
+                .build()
+            chain.proceed(newRequest)
+        }
+
+        return OkHttpClient.Builder().addInterceptor(interceptor).build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(okHttpClient: OkHttpClient): CatApi {
         return Retrofit.Builder()
             .baseUrl(BuildConfig.BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
             .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
+            .client(okHttpClient)
             .build()
             .create(CatApi::class.java)
     }
